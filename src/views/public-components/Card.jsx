43e0ef31abe140ca28b3d082d410b8ea8ecd3514
Ricardo-we <totgamer75@gmail.com>
2022-04-02 10:56:08 -0600

    Vertical/Horizontal cards components changed styles

--- src/views/public-components/Card.jsx
+++ src/views/public-components/Card.jsx
@@ -1,14 +1,14 @@
 export function HorizontalCard({ title, image, content, onClick }) {
     return ( 
-        <div className="card m-2" style={{minWidth: 200,maxWidth: 540, cursor: 'pointer'}} onClick={onClick}>
-            <div className="row g-0">
+        <div className="card m-2" style={{minWidth: 200, height: 240, maxWidth: 450, cursor: 'pointer'}} onClick={onClick}>
+            <div className="row g-0 h-100">
                 {image?
-                    <div className="col-md-4 d-flex align-items-center justify-content-center">
-                        <img src={image} className="img-fluid rounded-start" />
-                    </div>
+                    // <div className="col-md-4 d-flex align-items-center justify-content-center">
+                        <img src={image} style={{objectFit: 'cover'}} className="col-md-5 h-100 img-fluid rounded-start" />
+                    //</div> 
                 : null
                 }
-                <div className="col-md-8">
+                <div className="col-md-7">
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text" style={{overflow: 'hidden', maxHeight: 150}}>{content}</p>
@@ -25,7 +25,7 @@ export function VerticalCard({ title, image, content, onClick  }){
         <div 
             className="card m-3" 
             onClick={onClick} 
-            style={{maxWidth: 450, height: 350, overflow: 'hidden', cursor: 'pointer'}}
+            style={{minWidth:200,width: 300, height: 350, overflow: 'hidden', cursor: 'pointer'}}
         >
             {image? <img src={image} className="card-img-top" alt={title} style={{maxHeight: '200px', objectFit: 'cover'}}/>
             : ''
